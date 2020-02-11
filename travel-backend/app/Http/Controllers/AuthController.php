e451613901293e28f39949747ffb4b3c86b02474
bagusnusantara <bagus.nusantara313@gmail.com>
2020-02-11 10:20:08 +0700

    add auth controller

--- travel-backend/app/Http/Controllers/AuthController.php
+++ travel-backend/app/Http/Controllers/AuthController.php
@@ -13,35 +13,35 @@ class AuthController extends Controller
     public function login(Request $request)
     {
         $this->validate($request, [
-            'email' => 'required',
+            'email' => 'required', 
             'password' => 'required',
         ]);
 
         $user = User::where('email', '=', $request->email)->firstOrFail();
-        $status = false;
+        $status = "error";
         $message = "";
         $data = null;
         $code = 401;
         if($user){
             if (Hash::check($request->password, $user->password)) {
                 $user->generateToken();
-                $status = true;
+                $status = 'success';
                 $message = 'Login sukses';
                 $data = $user->toArray();
-                $code = 200;
+                $code = 200;  
             }
             else{
                 $message = "Login gagal, password salah";
-            }
+            }          
         }
         else{
             $message = "Login gagal, username salah";
         }
 
         return response()->json([
-            'isSuccesful' => $status,
+            'status' => $status,
             'message' => $message,
-            'user' => $data
+            'data' => $data
         ], $code);
     }
 
@@ -52,7 +52,7 @@ class AuthController extends Controller
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6',
         ]);
-
+        
         $status = "error";
         $message = "";
         $data = null;
@@ -69,7 +69,7 @@ class AuthController extends Controller
                 'roles'    => json_encode(['CUSTOMER']),
             ]);
             if($user){
-                //Auth::login($user);
+                Auth::login($user);
                 $user->generateToken();
                 $status = "success";
                 $message = "register successfully";
@@ -99,6 +99,6 @@ class AuthController extends Controller
             'status' => 'success',
             'message' => 'logout berhasil',
             'data' => []
-        ], 200);
+        ], 200); 
     }
 }
