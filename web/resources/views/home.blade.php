7287ee4825250d4503ed7fc79ffe96ae29d33c0a
bagusnusantara <bagus.nusantara313@gmail.com>
2020-02-25 13:04:31 +0700

    making a web template

--- web/resources/views/home.blade.php
+++ web/resources/views/home.blade.php
@@ -1,23 +1,58 @@
-@extends('layouts.app')
+@extends('layouts.admin.index')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+<!-- sales report area start -->
+<div class="sales-report-area mt-5 mb-5">
+    <div class="row">
+        <div class="col-md-4">
+            <div class="single-report mb-xs-30">
+                <div class="s-report-inner pr--20 pt--30 mb-3">
+                    <div class="icon"><i class="fa fa-btc"></i></div>
+                    <div class="s-report-title d-flex justify-content-between">
+                        <h4 class="header-title mb-0">Bitcoin</h4>
+                        <p>24 H</p>
+                    </div>
+                    <div class="d-flex justify-content-between pb-2">
+                        <h2>$ 4567809,987</h2>
+                        <span>- 45.87</span>
+                    </div>
                 </div>
+                <canvas id="coin_sales1" height="100"></canvas>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="single-report mb-xs-30">
+                <div class="s-report-inner pr--20 pt--30 mb-3">
+                    <div class="icon"><i class="fa fa-btc"></i></div>
+                    <div class="s-report-title d-flex justify-content-between">
+                        <h4 class="header-title mb-0">Bitcoin Dash</h4>
+                        <p>24 H</p>
+                    </div>
+                    <div class="d-flex justify-content-between pb-2">
+                        <h2>$ 4567809,987</h2>
+                        <span>- 45.87</span>
+                    </div>
+                </div>
+                <canvas id="coin_sales2" height="100"></canvas>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="single-report">
+                <div class="s-report-inner pr--20 pt--30 mb-3">
+                    <div class="icon"><i class="fa fa-eur"></i></div>
+                    <div class="s-report-title d-flex justify-content-between">
+                        <h4 class="header-title mb-0">Euthorium</h4>
+                        <p>24 H</p>
+                    </div>
+                    <div class="d-flex justify-content-between pb-2">
+                        <h2>$ 4567809,987</h2>
+                        <span>- 45.87</span>
+                    </div>
+                </div>
+                <canvas id="coin_sales3" height="100"></canvas>
             </div>
         </div>
     </div>
 </div>
+<!-- sales report area end -->
 @endsection
---
+++ web/resources/views/layouts/admin/sidebar.blade.php
@@ -0,0 +1,31 @@
+        <!-- sidebar menu area start -->
+        <div class="sidebar-menu">
+            <div class="sidebar-header">
+                <div class="logo">
+                    <a href="index.html"><img src="{{ asset('srtdash/assets/images/icon/logo.png') }}" alt="logo"></a>
+                </div>
+            </div>
+            <div class="main-menu">
+                <div class="menu-inner">
+                    <nav>
+                        <ul class="metismenu" id="menu">
+                            <li class="active">
+                                <a href="javascript:void(0)" aria-expanded="true"><i class="ti-dashboard"></i><span>dashboard</span></a>
+                                <ul class="collapse">
+                                    <li class="active"><a href="index.html">ICO dashboard</a></li>
+                                    <li><a href="index2.html">Ecommerce dashboard</a></li>
+                                    <li><a href="index3.html">SEO dashboard</a></li>
+                                </ul>
+                            </li>
+
+                            <li><a href="maps.html"><i class="ti-receipt"></i> <span>Payment History</span></a></li>
+                            <li><a href="invoice.html"><i class="ti-location-pin"></i> <span>Destinations</span></a></li>
+                            <li><a href="maps.html"><i class="ti-money"></i> <span>Price Management</span></a></li>
+                            <li><a href="invoice.html"><i class="ti-user"></i> <span>Users Management</span></a></li>
+                            
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+        <!-- sidebar menu area end -->
